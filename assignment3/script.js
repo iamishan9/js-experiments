ed2d1a4712f8d0ccba7f000e2653b974eb6d2364
iamishang <iamishan9@gmail.com>
2018-12-02 10:59:11 +0545

    assignment 3 almost completed

--- assignment3/script.js
+++ assignment3/script.js
@@ -1,42 +1,100 @@
-var gameContainer = document.getElementById('main-div');
 
+var birdy = document.getElementById('birdy');
+var sprite = document.getElementById('sprite');
+var spriteStepWidth = 92;
+var spriteSteps     = 2;
+var step            = 0;
 
-var bird = document.createElement('img');
-bird.style.left = "50px";
-var birdY = 250;
-var pressY = 0;
-var speed = 10;
-var gravity= 5;
+var scoreBoard = document.getElementById('score-board');
 
-bird.src = './images/bird.png';
+function animateSprite () {
+  sprite.style.left= -step * spriteStepWidth + 'px';
+  if (step == spriteSteps) {
+    step = 0;
+  }
+  else {
+    step++;
+  }
 
+}
+var up = false;
 
-var stopGame = function(){
+document.onkeydown = function (e) {
+  if ( e.keyCode == 32 ) {
+    up = true;
+  }
+}
+document.onkeyup = function (e) {
+  if ( e.keyCode == 32 ) {
+    up = false;
+  }
+}
+
+function animateFly () {
+  var fall = birdy.offsetTop;
+  if (up == true) {
+    birdy.style.top = (fall - 20) + 'px';
+  }
+  else {
+    birdy.style.top = (fall + 12) + 'px';
+  }
+
+}
+var pipes = document.getElementsByClassName('pipe');
+var pipeWidth = pipes[0].offsetWidth;
+
+function updateHeight (upPipe, downPipe) {
+   var obstacle = 70;
+   var blockObstacle = obstacle / 3;
+   var random = Math.floor( Math.random() * blockObstacle );
+   var upObstacle = blockObstacle + random;
+   var downObstacle = blockObstacle + (blockObstacle - random);
+
+   upPipe.style.height = upObstacle + '%';
+   downPipe.style.height = downObstacle + '%';
 
 }
 
-var runGame = function (){
+function touchPipe (pipe) {
+  var pipeLeft = pipe.offsetLeft;
+  var pipeRight = pipe.offsetLeft + pipe.offsetWidth;
+  var pipeUp = pipe.children[0].offsetTop+ pipe.children[0].offsetHeight;
+  var pipeDown = pipe.children[1].offsetTop;
+  var birdyyRight = birdy.offsetLeft + birdy.offsetWidth;
+  var birdyyLeft = birdy.offsetLeft;
+  var birdyyUp = birdy.offsetTop;
+  var birdyyDown = birdy.offsetHeight + birdy.offsetTop;
 
-
+  if ( birdyyRight > pipeLeft && birdyyLeft < pipeRight ) {
+    if (birdyyDown > pipeDown ) {
+      window.confirm("Game Over!");}
+    if (birdyyUp < pipeUp) {
+      window.confirm("Game Over");
+    }
+  }
   
-gameContainer.appendChild(bird);
-  birdY += gravity;
-  bird.style.top = birdY + "px";
-  document.addEventListener("keydown", moveUp);
-
-
-
-
-
-function moveUp(){
-  pressY = -50;
-  birdY += pressY;
-  bird.style.top = birdY + "px";
 
 }
 
+function animatePipes () {
+  
+  Array.prototype.forEach.call( pipes, function (pipe) {
+    
+    var left = pipe.offsetLeft;
+    pipe.style.left = left - 10 + 'px';
+    if (left < -pipeWidth) {
+      pipe.style.left = 100 + '%';
+      updateHeight(pipe.children[0], pipe.children[1]);
+    }
+    touchPipe (pipe);
+  });
+
 }
 
+function animateScene () {
+  animateSprite();
+  animateFly();
+  animatePipes();
+}
 
-setInterval(runGame, 1000);
-
+var animationInstance =setInterval(animateScene, 40);
