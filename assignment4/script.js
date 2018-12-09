bd5bebbec7255e2c1882bd91dbd77a7bc1bb6715
iamishang <iamishan9@gmail.com>
2018-12-09 12:49:36 +0545

    removed unnecessary variables

--- assignment4/script.js
+++ assignment4/script.js
@@ -4,7 +4,6 @@
  	var timerDiv = document.querySelector(".time");
 	var scoreDiv = document.querySelector(".score");
 	var child = document.getElementById('wordsDiv'); 
-	// var parent = document.getElementById('wrapper');
  	var points = 0;
  	var spans;
  	var typed;
@@ -36,7 +35,7 @@
 			}
 			
 				// xx +=x;
-				// wordsWrap.style.top = xx + 'px';
+				// child.style.top = xx + 'px';
     		if (seconds === 0) {
     			alert("Game over! Your score is " + points);
     			scoreDiv.innerHTML = "0";
@@ -54,7 +53,7 @@
   		words.innerHTML = "";
   		var random = Math.floor(Math.random() * (13 - 0 + 1)) + 0;
   		var wordArray = list[random].split("");
-  		for (var i = 0; i < wordArray.length; i++) { //building the words with spans around the letters
+  		for (var i = 0; i < wordArray.length; i++) {
   			var span = document.createElement("span");
   			span.classList.add("span");
   			span.innerHTML = wordArray[i];
@@ -77,8 +76,8 @@
   	function typing(e) {
   			typed = String.fromCharCode(e.which);
   			for (var i = 0; i < spans.length; i++) {
-  				if (spans[i].innerHTML === typed) { // if typed letter is the one from the word
-  					if (spans[i].classList.contains("bg")) { // if it already has class with the bacground color then check the next one
+  				if (spans[i].innerHTML === typed) { 
+  					if (spans[i].classList.contains("bg")) { 
   						continue;
   					} else if (spans[i].classList.contains("bg") === false && spans[i-1] === undefined || spans[i-1].classList.contains("bg") !== false ) { // if it dont have class, if it is not first letter or if the letter before it dont have class (this is done to avoid marking the letters who are not in order for being checked, for example if you have two "A"s so to avoid marking both of them if the first one is at the index 0 and second at index 5 for example)
   						spans[i].classList.add("bg");
@@ -87,19 +86,17 @@
   				}
   			}
   			var checker = 0;
-  			for (var j = 0; j < spans.length; j++) { //checking if all the letters are typed
+  			for (var j = 0; j < spans.length; j++) { 
   				if (spans[j].className === "span bg") {
   					checker++;
   				}
-  				if (checker === spans.length) { // if so, animate the words with animate.css class
-  					words.classList.add("animated");
-  					words.classList.add("fadeOut");
-  					points++; // increment the points
-  					scoreDiv.innerHTML = points; //add points to the points div
+  				if (checker === spans.length) {
+  					points++; 
+  					scoreDiv.innerHTML = points;
   					document.removeEventListener("keydown", typing, false);
   					setTimeout(function(){
-  						words.className = "words"; // restart the classes
-  						random(); // give another word
+  						words.className = "words"; 
+  						random(); 
   						document.addEventListener("keydown", typing, false);
   					}, 400);
   				}
